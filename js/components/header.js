6dfa7c4a90a555d89c371b409f8e09b8e106403b
Mishmore <michellemore28@gmail.com>
2017-06-06 11:23:50 -0500

    header ok

--- js/components/header.js
+++ js/components/header.js
@@ -4,7 +4,7 @@ var Header = (update) => {
   var row = $('<div class="row"></div>');
   var col = $('<div class="col s12"></div>')
   var title = $('<span>Gas Finder</span>');
-  var formGroup = $('<div class="form-group z-depth-2 col m12 col s12"></div>');
+  var formGroup = $('<div class="form-group z-depth-1 col m12 col s12"></div>');
   var icon = $('<i class="fa fa-search col s1 col m1" aria-hidden="true"></i>');
   var input = $('<input type="text" class="col s11 col m11" placeholder="Ingresa tu distrito a buscar">')
 
