5a3c6d27a7ad42b3ac75a3f0696b257a881cc0f0
Mishmore <michellemore28@gmail.com>
2017-06-06 10:44:54 -0500

    header css

--- js/components/header.js
+++ js/components/header.js
@@ -1,14 +1,20 @@
 var Header = (update) => {
   var header = $("<header></header>");
-  var title = $("<span>Gas Finder</span>");
-  var formGroup = 
-  var icon = $('<i class="fa fa-search" aria-hidden="true"></i>');
+  var container = $('<div class="container"></div>');
+  var row = $('<div class="row"></div>');
+  var col = $('<div class="col s12"></div>')
+  var title = $('<span>Gas Finder</span>');
+  var formGroup = $('<div class="form-group z-depth-2 col m12 col s12"></div>');
+  var icon = $('<i class="fa fa-search col s1 col m1" aria-hidden="true"></i>');
+  var input = $('<input type="text" class="col s11 col m11" placeholder="Ingresa tu distrito a buscar">')
 
-  var input = $('<input type="text" placeholder="Ingresa tu distrito a buscar">')
-
-  header.append(title);
-  header.append(icon);
-  header.append(input);
+  header.append(container);
+  container.append(row);
+  row.append(col);
+  col.append(title);
+  col.append(formGroup);
+  formGroup.append(icon);
+  formGroup.append(input);
 
   return header;
 }
