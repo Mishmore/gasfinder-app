5be76dcf7dba10888071f957ed6b0a60b01ebac2
Mishmore <michellemore28@gmail.com>
2017-06-06 12:23:37 -0500

    map y flex

--- js/components/search.js
+++ js/components/search.js
@@ -2,15 +2,19 @@
 var Search = (update) => {
   var container = $('<div class="collection"></div>');
   var divSearch = $('<div class="search-item collection-item grey-text text-darken-1"></div>');
+  var searchText = $('<div class="search-text"></div>');
   var title = $('<h6 class="black-text">Primax</h6>');
   var direccion = $('<p>Dirección</p>');
   var distrito = $('<p>Distrito</p>');
   var divider = $('<div class="divider grey lighten-7"></div>');
+  var map = $('<i class="fa fa-map" aria-hidden="true"></i>');
 
   container.append(divSearch);
-  divSearch.append(title);
-  divSearch.append(direccion);
-  divSearch.append(distrito);
+  divSearch.append(searchText);
+  searchText.append(title);
+  searchText.append(direccion);
+  searchText.append(distrito);
+  divSearch.append(map);
 
   return container;
 }
