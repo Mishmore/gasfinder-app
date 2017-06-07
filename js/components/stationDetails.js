5ef6ebb18a935823900c0726c1778af751bfb6fa
Mishmore <michellemore28@gmail.com>
2017-06-07 11:50:09 -0500

    items css

--- js/components/stationDetails.js
+++ js/components/stationDetails.js
@@ -1,21 +1,23 @@
 var StationDetails = (update) => {
-  var container = $('<div></div>');
+  var container = $('<div class="details"></div>');
   var map = $('<div id="map"></div>');
   var regresar = $('<i class="fa fa-chevron-left" aria-hidden="true"></i>');
-  var divDatos = $('<div><div>');
+  var row = $('<div class="row"></div>')
+  var divDatos = $('<div class="col s11 right"></div>');
   var stationName = $('<h5>' + state.selectedStation.name + '<h5>');
   var hr = $('<hr>');
   var stationAddress = $('<p>' + state.selectedStation.address + '</p>');
   var products = $('<ul></ul>');
 
   for (var i=0; i<state.selectedStation.products.length; i++) {
-    var productItem = $('<li>' + state.selectedStation.products[i] + '</li>');
+    var productItem = $('<li class="grey lighten-2">' + state.selectedStation.products[i] + '</li>');
     products.append(productItem);
   }
 
   container.append(regresar);
   container.append(map);
-  container.append(divDatos);
+  container.append(row);
+  row.append(divDatos);
   divDatos.append(stationName);
   divDatos.append(hr);
   divDatos.append(stationAddress);
